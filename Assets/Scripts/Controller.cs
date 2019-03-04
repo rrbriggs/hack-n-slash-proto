24a602859445453cf6c0b6dd06d94fa4e5053886
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-03-04 16:32:47 -0600

    add controller script
    add controller script
    update to 18.2.21f1

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public int Index { get; private set; }
+
+    private string attackButton;
+    private bool attack;
+
+    private void Start()
+    {
+        Index = 1;
+        attackButton = "Attack" + Index;
+    }
+
+    private void Update()
+    {
+        attack = Input.GetButton(attackButton);
+
+    }
+
+}
