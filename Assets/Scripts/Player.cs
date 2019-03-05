57c123a63ebd0471a85a7a4513792c565aa48eef
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-03-04 18:05:19 -0600

    Set 4 player UI Join text

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,14 +7,22 @@ public class Player : MonoBehaviour
     [SerializeField] private int playerNumber;
 
     private Controller controller;
+    private UIPlayerText uiPlayerText;
 
     public bool HasController { get { return controller != null; } }
     public int PlayerNumber { get { return playerNumber; } }
+
+    private void Awake()
+    {
+        uiPlayerText = GetComponentInChildren<UIPlayerText>();
+    }
 
     public void InitializePlayer(Controller controller)
     {
         this.controller = controller;
 
         gameObject.name = string.Format("Player {0} - {1}", playerNumber, controller.gameObject.name);
+
+        uiPlayerText.HandlePlayerInitialized();
     }
 }
