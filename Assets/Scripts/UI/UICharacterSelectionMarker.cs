8cbdc3435fb9987726e870cc4ac1b9bc53424f13
Ryan Briggs <ryan.rbriggs@gmail.com>
2019-03-04 18:59:09 -0600

    Begin work on character selection

---
+++ Assets/Scripts/UI/UICharacterSelectionMarker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICharacterSelectionMarker : MonoBehaviour
+{
+    [SerializeField] private Player player;
+    [SerializeField] private Image markerImage;
+    [SerializeField] private Image lockImage;
+
+    private UICharacterSelectionMenu menu;
+    private bool initializing;
+    private bool initialized;
+
+    private void Awake()
+    {
+        menu = GetComponentInParent<UICharacterSelectionMenu>();
+        markerImage.gameObject.SetActive(false);
+        lockImage.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (player.HasController == false)
+        {
+            return;
+        }
+
+        if (!initializing)
+        {
+            StartCoroutine(Initialize());
+        }
+
+        if (!initialized)
+        {
+            return;
+        }
+    }
+
+    private IEnumerator Initialize()
+    {
+        initializing = true;
+        yield return new WaitForSeconds(0.5f);
+        markerImage.gameObject.SetActive(true);
+        initialized = true;
+    }
+}
